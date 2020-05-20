bf155acb4578daf08c8ff63262fc7ccd1d366dbd
Omokay <chuku.omoke@gmail.com>
2020-05-20 23:13:42 +0100

    svg cleaning

--- src/Components/Header/header.component.jsx
+++ src/Components/Header/header.component.jsx
@@ -6,48 +6,97 @@ const Header = () => {
     return (
         <div className='header'>
             <nav>
-                <button><svg className='hamburger' width='29' height='18' viewBox='0 0 29 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                    <g filter='url(#filter0_d)'>
-                        <rect x='2' y='2' width='25' height='3' fill='black' />
-                    </g>
-                    <g filter='url(#filter1_d)'>
-                        <rect x='14' y='12' width='13' height='3' fill='black' />
-                    </g>
-                    <g filter='url(#filter2_d)'>
-                        <rect x='9' y='7' width='18' height='3' fill='black' />
-                    </g>
-                    <defs>
-                        <filter id='filter0_d' x='0' y='0.3' width='29' height='7' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'>
-                            <feFlood flood-opacity='0' result='BackgroundImageFix' />
-                            <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' />
-                            <feOffset dy='0.3' />
-                            <feGaussianBlur stdDeviation='1' />
-                            <feColorMatrix type='matrix' values='0 0 0 0 0.822917 0 0 0 0 0.839375 0 0 0 0 0.9875 0 0 0 1 0' />
-                            <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow' />
-                            <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow' result='shape' />
-                        </filter>
-                        <filter id='filter1_d' x='12' y='10.3' width='17' height='7' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'>
-                            <feFlood flood-opacity='0' result='BackgroundImageFix' />
-                            <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' />
-                            <feOffset dy='0.3' />
-                            <feGaussianBlur stdDeviation='1' />
-                            <feColorMatrix type='matrix' values='0 0 0 0 0.822917 0 0 0 0 0.839375 0 0 0 0 0.9875 0 0 0 1 0' />
-                            <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow' />
-                            <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow' result='shape' />
-                        </filter>
-                        <filter id='filter2_d' x='7' y='5.3' width='22' height='7' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'>
-                            <feFlood flood-opacity='0' result='BackgroundImageFix' />
-                            <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' />
-                            <feOffset dy='0.3' />
-                            <feGaussianBlur stdDeviation='1' />
-                            <feColorMatrix type='matrix' values='0 0 0 0 0.822917 0 0 0 0 0.839375 0 0 0 0 0.9875 0 0 0 1 0' />
-                            <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow' />
-                            <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow' result='shape' />
-                        </filter>
-                    </defs>
-                </svg>
+                <div className='nav-brand'>
+                    <svg className='nav-brand' width='50' height='50' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                        <g filter='url(#filter0_d)'>
+                            <rect className='blocks' width='6.93092' height='1.98267' transform='matrix(0.710397 -0.703801 0.71239 0.701784 15.5521 15.005)' fill='black' />
+                            <rect className='blocks' width='6.93092' height='1.98267' transform='matrix(0.710397 -0.703801 0.71239 0.701784 20.3809 19.7619)' fill='black' />
+                            <rect className='blocks' width='1.9826' height='8.66589' transform='matrix(0.710397 -0.703801 0.71239 0.701784 15.072 15.4807)' fill='black' />
+                            <rect className='blocks' width='8.66365' height='1.98267' transform='matrix(0.710397 -0.703801 0.71239 0.701784 10.136 20.3708)' fill='black' />
+                            <rect className='blocks' width='8.66365' height='1.98267' transform='matrix(0.710397 -0.703801 0.71239 0.701784 14.9648 25.1277)' fill='black' />
+                            <rect className='blocks' width='1.9826' height='8.77349' transform='matrix(0.710397 -0.703801 0.71239 0.701784 9.64368 20.8586)' fill='black' />
+                            <rect className='blocks' width='2.97163' height='2.54187' transform='matrix(0.710397 -0.703801 0.71239 0.701784 14.1688 20.5495)' fill='black' />
+                            <rect className='blocks' width='1.9826' height='2.88957' transform='matrix(0.710397 -0.703801 0.71239 0.701784 24.5176 14.3518)' fill='black' />
+                            <rect className='blocks' width='1.9826' height='2.88808' transform='matrix(0.710397 -0.703801 0.71239 0.701784 20.4758 10.1271)' fill='black' />
+                            <rect className='blocks' width='1.27356' height='0.847289' transform='matrix(0.710397 -0.703801 0.71239 0.701784 15.3756 20.5466)' fill='white' />
+                            <rect className='blocks' width='6.93092' height='1.98267' transform='matrix(0.710397 -0.703801 0.71239 0.701784 15.5521 15.005)' fill='black' />
+                            <rect className='blocks' width='6.93092' height='1.98267' transform='matrix(0.710397 -0.703801 0.71239 0.701784 20.3809 19.7619)' fill='black' />
+                            <rect className='blocks' width='1.9826' height='8.66589' transform='matrix(0.710397 -0.703801 0.71239 0.701784 15.072 15.4807)' fill='black' />
+                            <rect className='blocks' width='8.66365' height='1.98267' transform='matrix(0.710397 -0.703801 0.71239 0.701784 10.136 20.3708)' fill='black' />
+                            <rect className='blocks' width='8.66365' height='1.98267' transform='matrix(0.710397 -0.703801 0.71239 0.701784 14.9648 25.1277)' fill='black' />
+                            <rect className='blocks' width='1.9826' height='8.77349' transform='matrix(0.710397 -0.703801 0.71239 0.701784 9.64368 20.8586)' fill='black' />
+                            <rect className='blocks' width='2.97163' height='2.54187' transform='matrix(0.710397 -0.703801 0.71239 0.701784 14.1688 20.5495)' fill='black' />
+                            <rect className='blocks' width='1.9826' height='2.88957' transform='matrix(0.710397 -0.703801 0.71239 0.701784 24.5176 14.3518)' fill='black' />
+                            <rect className='blocks' width='1.9826' height='2.88808' transform='matrix(0.710397 -0.703801 0.71239 0.701784 20.4758 10.1271)' fill='black' />
+                            <rect className='blocks' width='1.27356' height='0.847289' transform='matrix(0.710397 -0.703801 0.71239 0.701784 15.3756 20.5466)' fill='white' />
+                            <rect className='blocks' width='6.93122' height='1.98258' transform='matrix(-0.712887 0.701279 -0.709898 -0.704305 15.4169 14.0275)' fill='black' />
+                            <rect className='blocks' width='6.93122' height='1.98258' transform='matrix(-0.712887 0.701279 -0.709898 -0.704305 10.6052 9.25369)' fill='black' />
+                            <rect className='blocks' width='1.98268' height='8.66552' transform='matrix(-0.712887 0.701279 -0.709898 -0.704305 15.8987 13.5536)' fill='black' />
+                            <rect className='blocks' width='8.66403' height='1.98258' transform='matrix(-0.712887 0.701279 -0.709898 -0.704305 20.8522 8.68069)' fill='black' />
+                            <rect className='blocks' width='8.66403' height='1.98258' transform='matrix(-0.712887 0.701279 -0.709898 -0.704305 16.0405 3.90689)' fill='black' />
+                            <rect className='blocks' width='1.98268' height='8.77312' transform='matrix(-0.712887 0.701279 -0.709898 -0.704305 21.3463 8.19464)' fill='black' />
+                            <rect className='blocks' width='2.97176' height='2.54176' transform='matrix(-0.712887 0.701279 -0.709898 -0.704305 16.8201 8.48785)' fill='black' />
+                            <rect className='blocks' width='1.98268' height='2.88945' transform='matrix(-0.712887 0.701279 -0.709898 -0.704305 6.4491 14.6493)' fill='black' />
+                            <rect className='blocks' width='1.98268' height='2.88796' transform='matrix(-0.712887 0.701279 -0.709898 -0.704305 10.4757 18.8882)' fill='black' />
+                            <rect className='blocks' width='1.27361' height='0.847252' transform='matrix(-0.712887 0.701279 -0.709898 -0.704305 15.6133 8.48657)' fill='white' />
+                        </g>
+                        <defs>
+                            <filter id='filter0_d' x='0.984436' y='0.315686' width='29.0001' height='29' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'>
+                                <feFlood flood-opacity='0' result='BackgroundImageFix' />
+                                <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' />
+                                <feOffset dy='0.3' />
+                                <feGaussianBlur stdDeviation='1' />
+                                <feColorMatrix type='matrix' values='0 0 0 0 0.822917 0 0 0 0 0.839375 0 0 0 0 0.9875 0 0 0 1 0' />
+                                <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow' />
+                                <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow' result='shape' />
+                            </filter>
+                        </defs>
+                    </svg>
+                </div>
+                <div className='hamburger'>
+                    <svg className='hamburger' width='35' height='35' viewBox='0 0 29 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                        <g filter='url(#filter0_d)'>
+                            <rect x='2' y='2' width='25' height='3' fill='black' />
+                        </g>
+                        <g filter='url(#filter1_d)'>
+                            <rect x='14' y='12' width='13' height='3' fill='black' />
+                        </g>
+                        <g filter='url(#filter2_d)'>
+                            <rect x='9' y='7' width='18' height='3' fill='black' />
+                        </g>
+                        <defs>
+                            <filter id='filter0_d' x='0' y='0.3' width='29' height='7' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'>
+                                <feFlood flood-opacity='0' result='BackgroundImageFix' />
+                                <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' />
+                                <feOffset dy='0.3' />
+                                <feGaussianBlur stdDeviation='1' />
+                                <feColorMatrix type='matrix' values='0 0 0 0 0.822917 0 0 0 0 0.839375 0 0 0 0 0.9875 0 0 0 1 0' />
+                                <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow' />
+                                <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow' result='shape' />
+                            </filter>
+                            <filter id='filter1_d' x='12' y='10.3' width='17' height='7' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'>
+                                <feFlood flood-opacity='0' result='BackgroundImageFix' />
+                                <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' />
+                                <feOffset dy='0.3' />
+                                <feGaussianBlur stdDeviation='1' />
+                                <feColorMatrix type='matrix' values='0 0 0 0 0.822917 0 0 0 0 0.839375 0 0 0 0 0.9875 0 0 0 1 0' />
+                                <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow' />
+                                <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow' result='shape' />
+                            </filter>
+                            <filter id='filter2_d' x='7' y='5.3' width='22' height='7' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'>
+                                <feFlood flood-opacity='0' result='BackgroundImageFix' />
+                                <feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' />
+                                <feOffset dy='0.3' />
+                                <feGaussianBlur stdDeviation='1' />
+                                <feColorMatrix type='matrix' values='0 0 0 0 0.822917 0 0 0 0 0.839375 0 0 0 0 0.9875 0 0 0 1 0' />
+                                <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow' />
+                                <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow' result='shape' />
+                            </filter>
+                        </defs>
+                    </svg>
 
-</button>
+
+                </div>
             </nav>
         </div>
     )
