5221b47f7e4f0bf8bc1950606785767adc3d4b5e
Omokay <chuku.omoke@gmail.com>
2020-07-01 23:33:29 +0100

    forcefully removed cursor pointer on ul element

--- src/Components/Header/header.component.jsx
+++ src/Components/Header/header.component.jsx
@@ -32,7 +32,7 @@ class Header extends Component {
                     <div className='nav-brand'>
 
                     </div>
-                    <div className='hamburger' onClick={this.handleClick}>
+                    <div className='hambg' onClick={this.handleClick}>
                         <svg className='hamburger' width='30' height='30' viewBox='0 0 25 13' fill='none' xmlns='http://www.w3.org/2000/svg'>
                             <rect width='25' height='3' fill='#292A2C' />
                             <rect x='12' y='10' width='13' height='3' fill='#292A2C' />
@@ -43,16 +43,11 @@ class Header extends Component {
                         {
                             navVisibility ?
                                 <ul className='navShowing'>
-                                    <li className='navItems'><a href='#'>Home</a></li>
-                                    <li className='navItems'><a href='#'>About me</a></li>
-                                    <li className='navItems'><a href='#'>Projects</a></li>
-                                    <li className='navItems'><a href='#'>Skills</a></li>
-                                    <li className='navItems'><a href='#'>Get in touch</a></li>
-                                    <li class="closeBtn"><svg className='hamburger' width='30' height='30' viewBox='0 0 25 13' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                                        <rect width='25' height='3' fill='#292A2C' />
-                                        <rect x='12' y='10' width='13' height='3' fill='#292A2C' />
-                                        <rect x='7' y='5' width='18' height='3' fill='#292A2C' />
-                                    </svg></li>
+                                    <li className='navItems'>Home</li>
+                                    <li className='navItems'>About me</li>
+                                    <li className='navItems'>Projects</li>
+                                    <li className='navItems'>Skills</li>
+                                    <li className='navItems'>Get in touch</li>
                                 </ul> : 
                                     null
                         }
