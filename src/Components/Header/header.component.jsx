08d26d1c7c52080b410a6e49a64ace83ded22f0e
Omokay <chuku.omoke@gmail.com>
2020-09-13 22:20:11 +0100

    more updates lol

--- src/Components/Header/header.component.jsx
+++ src/Components/Header/header.component.jsx
@@ -41,9 +41,9 @@ class Header extends Component {
                             <rect x='7' y='5' width='18' height='3' fill='#292A2C' />
                         </svg>
 
-                        {/* <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="2.20965" height="15.4675" transform="matrix(0.728885 0.684637 -0.68464 0.728881 11.0962 0.213196)" fill="white" />
-                            <rect width="15.4675" height="2.20965" transform="matrix(0.728885 0.684637 -0.68464 0.728881 1.72595 0.50647)" fill="white" />
+                        {/* <svg width='13' height='13' viewBox='0 0 13 13' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                            <rect width='2.20965' height='15.4675' transform='matrix(0.728885 0.684637 -0.68464 0.728881 11.0962 0.213196)' fill='white' />
+                            <rect width='15.4675' height='2.20965' transform='matrix(0.728885 0.684637 -0.68464 0.728881 1.72595 0.50647)' fill='white' />
                         </svg> */}
 
 
@@ -58,9 +58,9 @@ class Header extends Component {
                                     <li className='navItems'>Skills</li>
                                     <li className='navItems'>Get in touch</li>
                                     <li className='navClose text-center' onClick={this.closeNav}>
-                                        <svg width="19" height="19" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <rect width="2.20965" height="15.4675" transform="matrix(0.728885 0.684637 -0.68464 0.728881 11.0962 0.213196)" fill="white" />
-                                            <rect width="15.4675" height="2.20965" transform="matrix(0.728885 0.684637 -0.68464 0.728881 1.72595 0.50647)" fill="white" />
+                                        <svg width='19' height='19' viewBox='0 0 13 13' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                                            <rect width='2.20965' height='15.4675' transform='matrix(0.728885 0.684637 -0.68464 0.728881 11.0962 0.213196)' fill='white' />
+                                            <rect width='15.4675' height='2.20965' transform='matrix(0.728885 0.684637 -0.68464 0.728881 1.72595 0.50647)' fill='white' />
                                         </svg>
                                     </li>
                                 </ul> : 
--- src/Components/Home/home.component.jsx
+++ src/Components/Home/home.component.jsx
@@ -1,34 +1,47 @@
-import React from "react";
+import React from 'react';
 
-import { Twitter } from "react-feather";
-import { Linkedin } from "react-feather";
-import { Instagram } from "react-feather";
-import { Youtube } from "react-feather";
-import "./home.styles.scss";
+import { Twitter } from 'react-feather';
+import { Linkedin } from 'react-feather';
+import { Instagram } from 'react-feather';
+import { Youtube } from 'react-feather';
+import ActionButton from '../ActionButton/actionButton.component';
+import ActionButton2 from '../ActionButton2/actionButton2.component';
+import './home.styles.scss';
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className=" row rrow">
+    <div className='home'>
+      <div className=' row rrow'>
         <div className='col-md-8 column  align-self-center'>
-          <div className="left-text">
-            <h2 className="main-text">Hi,
+          <div className='left-text'>
+            <h2 className='main-text'>Hi,
                       My name is Omoke.</h2>
-            <div className="sub-text">I am a Software Developer</div>
-            <p className="bio-text"></p>
+            <div className='sub-text'>I am a Software Developer</div>
+            <div className='action-buttons'>
+              <div className='learn-more'>
+                <ActionButton>Learn More</ActionButton>
+              </div>
+              <div className='hire-gig'>
+                <ActionButton2>Hire me for a gig</ActionButton2>
+              </div>
+            </div>
           </div>
         </div>
-        <div className='col-md-4 column'>
+        <div className='col-md-4 right-text'>
           
 
         </div>
       </div>
-      <p className="follow-text text-center">Follow me</p>
-      <div className="sm-list">
-        <Twitter className="icons" />
-        <Linkedin className="icons" />
-        <Instagram className="icons" />
-        <Youtube className="icons" />
+      <div className='row'>
+        <div className='col-md-12 text-center'>
+          <p className='follow-text text-center'>Follow me</p>
+          <div className='sm-list'>
+            <Twitter className='icons' />
+            <Linkedin className='icons' />
+            <Instagram className='icons' />
+            <Youtube className='icons' />
+          </div>
+        </div>
       </div>
     </div>
   );
