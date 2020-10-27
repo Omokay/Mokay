70540218a9d466512376f7b5c8481da75d479fe4
Omokay <chuku.omoke@gmail.com>
2020-10-27 13:18:08 +0100

    update latest

---
+++ src/Components/Projects/projects.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './projects.styles.scss';
+
+const project = () => {
+    return( 
+        <div>
+            <div className='row'>
+                <div className='col-sm-4'>
+
+                </div>
+                <div className='col-sm-4'>
+
+                </div>
+                <div className='col-sm-4'>
+
+                </div>
+            </div>
+            
+        </div>
+    )
+};
